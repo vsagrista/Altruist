2c225a218c52d9afabf139037c9acd41dfb20a00
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-30 16:54:47 +0200

    Made Transaction migration

--- db/migrate/20150930144357_create_transactions.rb
+++ db/migrate/20150930144357_create_transactions.rb
@@ -1,7 +1,12 @@
 class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
-
+      t.integer :creator_id
+      t.references :user, index: true
+      t.integer  :skill_id
+      t.integer :rating, :default => 0
+      t.integer :minutes, :default => 60
+      t.boolean :completed, :default => false
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930081738) do
+ActiveRecord::Schema.define(version: 20150930144357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,19 @@ ActiveRecord::Schema.define(version: 20150930081738) do
   end
 
   add_index "skills", ["user_id"], name: "index_skills_on_user_id", using: :btree
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "creator_id"
+    t.integer  "user_id"
+    t.integer  "skill_id"
+    t.integer  "rating",     default: 0
+    t.integer  "minutes",    default: 60
+    t.boolean  "completed",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "transactions", ["user_id"], name: "index_transactions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
