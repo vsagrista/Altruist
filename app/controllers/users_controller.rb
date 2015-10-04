105d65678432cf8de5529d0d027846b3763a35e7
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-10-04 20:30:58 +0200

    Got rid of conflicting CSS from calendar gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
       @transaction_created = Transaction.where(creator_id: params[:id])
       @transaction_completed = Transaction.where(user_id: params[:id], completed: true)
       @any_created_transaction_pending = @transaction_created.select { |t| !t.completed }.any?
+      @meetings = Meeting.all
     else
       render :error
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002113421) do
+ActiveRecord::Schema.define(version: 20151004175800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetings", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "skills", force: :cascade do |t|
     t.integer  "user_id"
