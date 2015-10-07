70e07c344d905063a99ea62fe8645543c63deeab
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-10-08 00:06:47 +0200

    Changed and added some variables to make dashboard more user friendly

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,27 +1,31 @@
 module UsersHelper
 
   def find_creator(transaction)
-    return User.find(transaction.creator_id).name.capitalize
+    User.find(transaction.creator_id).name.capitalize
   end
 
   def find_receiver(transaction)
-    return User.find(transaction.user_id).name.capitalize
+    User.find(transaction.user_id).name.capitalize
+  end
+
+  def find_skill_from_transaction(transaction)
+    Skill.find(transaction.skill_id)
   end
 
   def find_receiver_from_skill(skill)
-    return User.find(skill.user_id).name 
+    User.find(skill.user_id).name 
   end
 
   def find_transaction_from_date(date)
-    return Transaction.where(date: date)
+    Transaction.where(date: date)
   end
 
   def find_transactions_where_user_is_receiver(user_id)
-    return Transaction.where(user_id: user_id)
+    Transaction.where(user_id: user_id)
   end
 
   def find_transactions_where_user_is_creator(user_id)
-    return Transaction.where(creator_id: user_id)
+    Transaction.where(creator_id: user_id)
   end
 
   def get_all_skills_with_unique_cities
